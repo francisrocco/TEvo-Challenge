495d524230518b918aceed9fb4f2c11cca7c4133
Francis Rocco <roccofrancis@gmail.com>
2017-02-02 16:40:51 -0500

    add test class

---
+++ test_file.rb
@@ -0,0 +1,39 @@
+class Test
+
+  CLASSIC_KEYS: {
+    2 => "abc"
+    3 => "def"
+    4 => "ghi"
+    5 => "jkl"
+    6 => "mno"
+    7 => "pqrs"
+    8 => "tuv"
+    9 => "wxyz"
+  }
+
+  FRANCIS_KEYS: {
+    2 => "ehg"
+    3 => "tlf"
+    4 => "adp"
+    5 => "oub"
+    6 => "imv"
+    7 => "nwk"
+    8 => "syjq"
+    9 => "rcxz"
+  }
+
+  def test_case(word)
+    letters = word.split("")
+    classic_key_array =
+    francis_key_array =
+  end
+
+  def find_classic_key(letter)
+
+  end
+
+  def find_francis_key(letter)
+    
+  end
+
+end
