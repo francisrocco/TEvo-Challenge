8390dd97187aeeb66920445b3a2b8bacae373d15
Francis Rocco <roccofrancis@gmail.com>
2017-02-02 17:19:08 -0500

    add test_file and add to run file

--- run_francis_rocco.rb
+++ run_francis_rocco.rb
@@ -1,4 +1,19 @@
 require './challenge.rb'
+require './test_file.rb'
 
 francis = Challenge.new
 puts francis.run("michelle.txt", "chimamanda.txt")
+
+puts ""
+
+test1_word = "francis"
+test1 = Test.new(test1_word)
+puts "#{test1_word} #{test1.test_case} with #{test1.total_francis_presses} new presses vs. #{test1.total_classic_presses} classic presses"
+
+test2_word = "ticket evolution"
+test2 = Test.new(test2_word)
+puts "#{test2_word} #{test2.test_case} with #{test2.total_francis_presses} new presses vs. #{test2.total_classic_presses} classic presses"
+
+test3_word = "hire me"
+test3 = Test.new(test3_word)
+puts "#{test3_word} #{test3.test_case} with #{test3.total_francis_presses} new presses vs. #{test3.total_classic_presses} classic presses"
--- test_file.rb
+++ test_file.rb
@@ -1,39 +1,82 @@
 class Test
 
-  CLASSIC_KEYS: {
-    2 => "abc"
-    3 => "def"
-    4 => "ghi"
-    5 => "jkl"
-    6 => "mno"
-    7 => "pqrs"
-    8 => "tuv"
+  CLASSIC_KEYS = {
+    2 => "abc",
+    3 => "def",
+    4 => "ghi",
+    5 => "jkl",
+    6 => "mno",
+    7 => "pqrs",
+    8 => "tuv",
     9 => "wxyz"
   }
 
-  FRANCIS_KEYS: {
-    2 => "ehg"
-    3 => "tlf"
-    4 => "adp"
-    5 => "oub"
-    6 => "imv"
-    7 => "nwk"
-    8 => "syjq"
+  FRANCIS_KEYS = {
+    2 => "ehg",
+    3 => "tlf",
+    4 => "adp",
+    5 => "oub",
+    6 => "imv",
+    7 => "nwk",
+    8 => "syjq",
     9 => "rcxz"
   }
 
-  def test_case(word)
+  attr_reader :word
+
+  def initialize(word)
+    @word = word.gsub(/\s+/, "")
+  end
+
+  def test_case
     letters = word.split("")
-    classic_key_array =
-    francis_key_array =
+
+    total_classic_presses = classic_key_presses.inject(:+)
+    total_francis_presses = francis_key_presses.inject(:+)
+
+    if total_francis_presses < total_classic_presses
+      return "passes"
+    else
+      return "fails"
+    end
   end
 
   def find_classic_key(letter)
-
+    CLASSIC_KEYS.select{|key, value| value.include?(letter)}.values
   end
 
   def find_francis_key(letter)
-    
+    FRANCIS_KEYS.select{|key, value| value.include?(letter)}.values
+  end
+
+  def index_of_letter(letter_group, letter)
+    letter_group.index(letter)
+  end
+
+  def classic_key_presses
+    presses = []
+    word.split("").each do |letter|
+      letter_group = find_classic_key(letter)
+      presses << ((index_of_letter(letter_group[0], letter)) + 1)
+    end
+    return presses
+  end
+
+  def total_classic_presses
+    classic_key_presses.inject(:+)
+  end
+
+  def francis_key_presses
+    presses = []
+    word.split("").each do |letter|
+      letter_group = find_francis_key(letter)
+      presses << ((index_of_letter(letter_group[0], letter)) + 1)
+    end
+    return presses
+  end
+
+  def total_francis_presses
+    francis_key_presses.inject(:+)
   end
 
 end
